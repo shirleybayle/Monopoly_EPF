4f7dbfba45070bf9ab7237a44cf903a1ba833179
titou <titou@DESKTOP-AHI02ML.education.lakanal.epf.fr>
2022-02-21 16:05:27 +0100

    création de la classe joueur sans l'arraylist en attributs parce que je savais plus exactement ce que c'était!

---
+++ src/monopoly_epf/Joueur.java
@@ -0,0 +1,32 @@
+/*
+ * BOCAHUT Manon - FEVRIER Titouan - CHAUVET Ylan - BAYLE Shirley
+ */
+package monopoly_epf;
+
+/**
+ *
+ * @author titou
+ */
+public class Joueur {
+    int credits;
+    Pion pion;
+    
+    Carte [] tabCartes = new Carte [2];
+    String nom;
+    
+    public boolean acheter(Case caseassociee) {
+        
+    }
+    
+    public boolean construire(Case casemaison, int nbmaisons) {
+        
+    }
+    
+    public boolean jouerlacarte(Carte cartejoueur) {
+        
+    }
+    
+    public void recuperercarte(Carte cartearecuperer) {
+        
+    }
+}
