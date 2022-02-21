b7ccd293af56d5b55562a4d562f4bea1661b7fdc
titou <titou@DESKTOP-AHI02ML.education.lakanal.epf.fr>
2022-02-21 15:24:51 +0100

    création de la classe dés et plateau

---
+++ src/monopoly_epf/Des.java
@@ -0,0 +1,16 @@
+/*
+ * BOCAHUT Manon - FEVRIER Titouan - CHAUVET Ylan - BAYLE Shirley
+ */
+package monopoly_epf;
+
+/**
+ *
+ * @author titou
+ */
+public class Des {
+    int valeur;
+    
+    int lirevaleur() {
+        return(valeur);
+    }
+}
