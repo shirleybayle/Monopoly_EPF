183433d09c149a5a033d9701e22a17e77b1a04ba
titou <titou@DESKTOP-AHI02ML.education.lakanal.epf.fr>
2022-02-21 15:13:20 +0100

    création classe pion

---
+++ src/monopoly_epf/Pion.java
@@ -0,0 +1,21 @@
+/*
+ * BOCAHUT Manon - FEVRIER Titouan - CHAUVET Ylan - BAYLE Shirley
+ */
+package monopoly_epf;
+
+/**
+ *
+ * @author titou
+ */
+public class Pion {
+    String modèle;
+    Case case associée;
+    
+    void avancer(int nbcases) {
+        
+    }
+    
+    void téléportation(Carte cartetirée) {
+        
+    }
+}
