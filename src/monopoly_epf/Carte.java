9807f66ff4725b5d02137bc99fa4fb99c2cb9307
shirl <shirl@DESKTOP-FJJIUNF.education.lakanal.epf.fr>
2022-02-21 15:12:23 +0100

    ajout de la classe carte

---
+++ src/monopoly_epf/Carte.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package monopoly_epf;
+
+/**
+ *
+ * @author shirl
+ */
+public class Carte {
+    boolean chance;
+    boolean communaute;
+    String texte;
+    
+    public String lireCarte(){
+        return texte;
+    }
+    
+}
